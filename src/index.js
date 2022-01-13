ac38c4fd336e76f25ef59de0299a7715ee5727b1
Moriah Close <moriah.v.close@gmail.com>
2022-01-12 23:18:23 -0800

    add tailwind

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className='bg-slate-900 text-slate-300'>
+      <h1 className='font-bold text-6xl'>Just a Test</h1>
+
+      <p className='text-5xl'>
+      I gave it a cold? I gave it a virus. A computer virus. Yes, Yes, without the oops! Checkmate... Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should. Forget the fat lady! You're obsessed with the fat lady! Drive us out of here!
+     </p>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
